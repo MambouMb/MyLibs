4b6e981046e6e980021753ddae526de57bacf196
GRAND william <mwillgates@gmail.com>
2021-10-18 14:20:55 +0100

    Add project files.

---
+++ MyLibs.Serialisation/Serializer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace MyLibs.Serialisation
+{
+    public class Serializer
+    {
+        public void Serialize<T>(T data, string path)
+        {
+            using (StreamWriter file = new StreamWriter(path, true))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(file.BaseStream, data);
+            }
+        }
+        public T Deserialize<T>(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                return (T)bf.Deserialize(file.BaseStream);
+            }
+        }
+    }
+}
